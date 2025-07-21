71e3494c9570559b2eb3e26c2dfaf2baf3e09f6e
Kanishka Gupta <150531613+kanishkagupta23@users.noreply.github.com>
2025-07-21 23:14:26 +0530

    Miniproject02.py

--- DataStructure/miniproject02.py
+++ DataStructure/miniproject02.py
@@ -1,13 +1,11 @@
 #Given a list of participant scores for your University Sports Day, determine the score of the runner-up (the second highest unique score)
 
-# List of scores
-scores = [2, 3, 6, 6, 5]
+scores = [1, 3, 4, 4, 9]
 
-# Remove duplicates and sort in descending order
 unique_scores = list(set(scores))
 unique_scores.sort(reverse=True)
 
-# The runner-up score is the second element
+
 runner_up = unique_scores[1]
 
 print(runner_up)
