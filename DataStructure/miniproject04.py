bec31873ddb4430c8921caf77426a8b788f3f1bb
Kanishka Gupta <150531613+kanishkagupta23@users.noreply.github.com>
2025-07-21 23:17:18 +0530

    Miniproject04.py

--- DataStructure/miniproject04.py
+++ DataStructure/miniproject04.py
@@ -1,11 +1,8 @@
 # Input string
 input_string = "Hi Alex WelcomeAlex Bye Alex."
 
-# Substring to count
 name_to_count = "Alex"
 
-# Counting occurrences
 count = input_string.count(name_to_count)
 
-# Output the result
 print(count)
